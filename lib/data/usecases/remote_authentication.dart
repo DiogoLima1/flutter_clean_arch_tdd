58f548979d871ee72fca6ad41c059fe1a29d3ffa
Diogo Costa <diogo.lima>
2022-05-06 14:43:54 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -3,15 +3,39 @@ import 'package:meta/meta.dart';
 import '../../data/http/http.dart';
 
 import '../../domain/usecases/usecases.dart';
+import '../../domain/helpers/helpers.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
+
   RemoteAuthentication({
     @required this.httpClient,
     @required this.url,
   });
+
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
+
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    @required this.email,
+    @required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(email: params.email, password: params.secret);
+
+  Map toJson() => {'email': email, 'password': password};
 }
