0c391ddf8693b96cae84fb56b8f374bad65fa554
Diogo Costa <diogo.lima>
2022-05-08 09:23:23 -0300

    ensure HttpAdapter calls post with correct Headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -13,7 +13,11 @@ class HttpAdapter extends Mock implements Client {
     @required String method,
     Map body,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -30,7 +34,10 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(url, headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      }));
     });
   });
 }
