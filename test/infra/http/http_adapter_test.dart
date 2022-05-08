2ee735f406455b2989be243815eb2ec5082c578d
Diogo Costa <diogo.lima>
2022-05-08 10:41:11 -0300

    ensure HttpAdapter throws BadRequestError on 400 with null body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -106,5 +106,17 @@ void main() {
 
       expect(future, throwsA(HttpError.badRequest));
     });
+
+    test('Should return BadRequestError if post returns 400 with null body',
+        () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
   });
 }
