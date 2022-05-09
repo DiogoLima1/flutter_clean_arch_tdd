c75959fa0fd9c6b5dda813fe634b9a1dedf79dfa
Diogo Costa <diogo.lima>
2022-05-08 21:24:54 -0300

    ensure LoginPage present loading

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -13,64 +13,91 @@ class LoginPage extends StatelessWidget {
         elevation: 0,
         title: const Text(''),
       ),
-      body: SingleChildScrollView(
-          child: Center(
-        child: Column(
-          children: [
-            Container(
-              child:
-                  ListTile(leading: Icon(Icons.dehaze), title: Text('Login')),
+      body: Builder(
+        builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading) {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  child: SimpleDialog(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircularProgressIndicator(),
+                          SizedBox(height: 10),
+                          Text(
+                            'Aguarde...',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    ],
+                  ));
+            }
+          });
+
+          return SingleChildScrollView(
+              child: Center(
+            child: Column(
+              children: [
+                Container(
+                  child: ListTile(
+                      leading: Icon(Icons.dehaze), title: Text('Login')),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      StreamBuilder<String>(
+                          stream: presenter.emailErrorStream,
+                          builder: (context, snapshot) {
+                            return TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  icon: Icon(Icons.email),
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data),
+                              onChanged: presenter.validateEmail,
+                              keyboardType: TextInputType.emailAddress,
+                            );
+                          }),
+                      StreamBuilder<String>(
+                          stream: presenter.passwordErrorStream,
+                          builder: (context, snapshot) {
+                            return TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Senha',
+                                  icon: Icon(Icons.lock),
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data),
+                              onChanged: presenter.validatePassword,
+                              obscureText: true,
+                            );
+                          }),
+                      StreamBuilder<bool>(
+                          stream: presenter.isFormValidStream,
+                          builder: (context, snapshot) {
+                            return RaisedButton(
+                              onPressed:
+                                  snapshot.data == true ? presenter.auth : null,
+                              child: Text('Entrar'.toUpperCase()),
+                            );
+                          }),
+                      FlatButton.icon(
+                          onPressed: null,
+                          icon: Icon(Icons.person),
+                          label: Text('Criar Conta'))
+                    ],
+                  ),
+                )
+              ],
             ),
-            Form(
-              child: Column(
-                children: [
-                  StreamBuilder<String>(
-                      stream: presenter.emailErrorStream,
-                      builder: (context, snapshot) {
-                        return TextFormField(
-                          decoration: InputDecoration(
-                              labelText: 'Email',
-                              icon: Icon(Icons.email),
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data),
-                          onChanged: presenter.validateEmail,
-                          keyboardType: TextInputType.emailAddress,
-                        );
-                      }),
-                  StreamBuilder<String>(
-                      stream: presenter.passwordErrorStream,
-                      builder: (context, snapshot) {
-                        return TextFormField(
-                          decoration: InputDecoration(
-                              labelText: 'Senha',
-                              icon: Icon(Icons.lock),
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data),
-                          onChanged: presenter.validatePassword,
-                          obscureText: true,
-                        );
-                      }),
-                  StreamBuilder<bool>(
-                      stream: presenter.isFormValidStream,
-                      builder: (context, snapshot) {
-                        return RaisedButton(
-                          onPressed:
-                              snapshot.data == true ? presenter.auth : null,
-                          child: Text('Entrar'.toUpperCase()),
-                        );
-                      }),
-                  FlatButton.icon(
-                      onPressed: null,
-                      icon: Icon(Icons.person),
-                      label: Text('Criar Conta'))
-                ],
-              ),
-            )
-          ],
-        ),
-      )),
+          ));
+        },
+      ),
     );
   }
 }
